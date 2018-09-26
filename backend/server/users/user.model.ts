48546653f0fdbf9c092ebd69d1243461fd47c448
Chiciuc Nicușor <nicuchiciuc@gmail.com>
2018-09-26 10:43:41 +0300

    Small changes

--- backend/server/users/user.model.ts
+++ backend/server/users/user.model.ts
@@ -1,15 +1,15 @@
-import * as crypto from "crypto";
-import * as jwt from "jsonwebtoken";
-import * as mongoose from "mongoose";
+import * as crypto from 'crypto'
+import * as jwt from 'jsonwebtoken'
+import * as mongoose from 'mongoose'
 
 // Declare the model interface
 interface User extends mongoose.Document {
-  email: string;
-  hash: string;
-  salt: string;
-  setPassword(password: string): void;
-  isPasswordValid(password: string): boolean;
-  generateJwt(): { token: string; expiry: Date };
+  email: string
+  hash: string
+  salt: string
+  setPassword(password: string): void
+  isPasswordValid(password: string): boolean
+  generateJwt(): { token: string; expiry: Date }
 }
 
 // Declare the model schema
@@ -18,53 +18,60 @@ const userSchema = new mongoose.Schema({
     type: String,
     // Important! We want users to be unique
     unique: true,
-    required: true
+    required: true,
   },
   hash: {
     type: String,
-    required: true
+    required: true,
   },
   salt: {
     type: String,
-    required: true
-  }
-});
+    required: true,
+  },
+})
 
 // Define some public methods for our model
 class UserClass {
-  private _id: string;
-  private email: string;
-  private salt: string;
-  private hash: string;
+  private _id: string
+  private email: string
+  private salt: string
+  private hash: string
 
   // Create a salt and hash from the password
   public setPassword(password: string) {
-    this.salt = crypto.randomBytes(16).toString("hex");
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 100000, 512, "sha512").toString("hex");
+    this.salt = crypto.randomBytes(16).toString('hex')
+    this.hash = crypto
+      .pbkdf2Sync(password, this.salt, 100000, 512, 'sha512')
+      .toString('hex')
   }
 
   // Check if hashes match
   public isPasswordValid(password: string): boolean {
-    const hash = crypto.pbkdf2Sync(password, this.salt, 100000, 512, "sha512").toString("hex");
-    return this.hash === hash;
+    const hash = crypto
+      .pbkdf2Sync(password, this.salt, 100000, 512, 'sha512')
+      .toString('hex')
+    return this.hash === hash
   }
 
   // Generate access token for 30 minutes
   public generateJwt(): { token: string; expiry: Date } {
-    const expiry = new Date();
-    expiry.setMinutes(expiry.getMinutes() + 30);
+    const expiry = new Date()
+    expiry.setMinutes(expiry.getMinutes() + 30)
 
-    const token = jwt.sign({
-      _id: this._id,
-      email: this.email,
-      exp: Math.round(expiry.getTime() / 1000),
-    }, process.env.AUTH_SHARED_SECRET);
+    const token = jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        exp: Math.round(expiry.getTime() / 1000),
+      },
+      process.env.AUTH_SHARED_SECRET
+    )
 
-    return { token, expiry };
+    return { token, expiry }
   }
 }
 
 // Important! Don't forget to use loadClass so your new methods will be included in the model
-userSchema.loadClass(UserClass);
+userSchema.loadClass(UserClass)
 
-export default mongoose.model<User>("User", userSchema);
+export default mongoose.model<User>('User', userSchema)
